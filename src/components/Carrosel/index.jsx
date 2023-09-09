fd574b0cf68bdab1f39bc3e9f5b6de27c119342c
Hugo Gonzalez <hugo11gonzalez@gmail.com>
2023-09-08 23:08:44 -0300

    2do commit

--- src/components/Carrosel/index.jsx
+++ src/components/Carrosel/index.jsx
@@ -1,17 +1,34 @@
 import React from 'react'
 import Card from '../Card'
 
+const cardsData = [
+  {id: 1, title: 'CARD 1', content: 'Clark Kent', imgUrl: 'https://unsplash.it/200/200'},
+  {id: 2, title: 'CARD 2', content: 'Bruce Wayne', imgUrl: 'https://unsplash.it/201/200'},
+  {id: 3, title: 'CARD 3', content: 'Peter Parker', imgUrl: 'https://unsplash.it/200/201'},
+  {id: 4, title: 'CARD 4', content: 'Tony Stark', imgUrl: 'https://unsplash.it/201/201'},
+  {id: 5, title: 'CARD 5', content: 'Reed Richards', imgUrl: 'https://unsplash.it/202/200'},
+  {id: 6, title: 'CARD 6', content: 'Wade Wilson', imgUrl: 'https://unsplash.it/200/199'},
+  {id: 7, title: 'CARD 7', content: 'Peter Quill', imgUrl: 'https://unsplash.it/199/199'},
+  {id: 8, title: 'CARD 8', content: 'Steven Rogers', imgUrl: 'https://unsplash.it/199/200'},
+  {id: 9, title: 'CARD 9', content: 'Bruce Banner', imgUrl: 'https://unsplash.it/200/198'},
+  {id: 10, title: 'CARD 10', content: 'Vincent Strange', imgUrl: 'https://unsplash.it/198/199'},
+]
+
 const index = (props) => {
   return (
-    <div className="cards-container">
-    {
-      props.cards.map((card) => (
-        <Card title={ card.title }
-          content={ card.content }
-          imgUrl={ card.imgUrl } />
-      ))
-    }
-  </div>
+      <div className=" d-flex flex-wrap gap-4 cards-container">
+        {
+          cardsData.map((card) => (
+            <Card
+              key={ card.id }
+              title={ card.title }
+              content={ card.content }
+              imgUrl={ card.imgUrl } 
+            />
+
+          ))
+        }
+      </div>
   )
 }
 
